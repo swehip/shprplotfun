122761d60529b29984c000a3f293bba97a6f3a53
Erik Bulow <eribu@rcvg.local>
2020-03-11 12:56:43 +0100

    Uppmuntrar registrernig av Arial Narrow via extrafont::font-import.
    Fix #3.

---
+++ R/zzz.R
@@ -0,0 +1,27 @@
+
+# Exporting figures to PNG might be problematic since non-standard fonts are
+# used by functions in the package
+.onLoad <- function(libname, pkgname) {
+
+  ask <- function(msg) utils::askYesNo(msg, prompts = "Y/N/C")
+
+  # Just warn if package 'extrafont' is not installed
+  # Thus, since it is already a suggested package, assume that the decision to
+  # not install was actively considered
+  if (!requireNamespace("extrafont", quietly = TRUE)) {
+    warning("Package 'extrafont' not installed!
+            This might cause problems with fonts if you export figures to PNG!")
+
+  # If the package is installed, we can assume that the intention has been to
+  # make relevant fonts available
+  } else if (
+      interactive() &&
+      !"Arial Narrow" %in% extrafont::fonts() &&
+      ask("Register 'Arial Narrow' for correct export to PNG?")) {
+    extrafont::font_import()
+    if (exists(".rs.restartR") &&
+      ask("Restart R to make fonts available?")) {
+      call(".rs.restartR")
+    }
+  }
+}
